23878e64bad9439cf96880a8656f068de14a423b
Renan Costa Alencar <renan.costaalencar@gmail.com>
2018-08-05 12:57:47 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -28,6 +28,9 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos = new ArrayList<>();
+
     public Cliente() {
 
     }
@@ -97,6 +100,15 @@ public class Cliente implements Serializable {
         this.telefones = telefones;
     }
 
+    @OneToMany(mappedBy = "cliente")
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -110,4 +122,5 @@ public class Cliente implements Serializable {
 
         return Objects.hash(id);
     }
+
 }
